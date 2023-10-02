efdeaf3320fbe9c0d9c1d2969d94835f1c660c2c
benhexie <benedictgabriel73@gmail.com>
2023-10-02 09:14:15 +0100

    Added loading to product page

--- src/pages/Home/Product/index.js
+++ src/pages/Home/Product/index.js
@@ -2,6 +2,7 @@ import { Link, useLocation, useNavigate } from "react-router-dom"
 import "./Product.css"
 import { useEffect, useState } from "react"
 import { FaAngleLeft, FaEnvelope, FaLocationArrow, FaPhone } from "react-icons/fa"
+import Loading from "../../../components/Loading";
 
 const SERVER = process.env.REACT_APP_SERVER;
 
@@ -34,50 +35,55 @@ const Product = () => {
   }
 
   return (
-    <div className="product-page">
-      <div className="product__container">
-        <div className="product__content">
-          <button className="btn back__btn" onClick={goback}>
-            <FaAngleLeft className="icon" />
-          </button>
-          <div className="image__container">
-            <img src={product.image} />
-          </div>
-          <section className="product__details__section">
-            <p className="title">{product.name}</p>
-            <p>₦ {product.price?.toLocaleString("en-US")}</p>
-            <p className="title">Description</p>
-            <p>{product.description}</p>
-          </section>
-        </div>
-        <div className="contact__container">
-          <section className="contact__details__item contact__image__section">
-            <div className="image__container">
-              <img src={product.user_image} />
+    <>
+      {
+        loading ? <Loading /> :
+        <div className="product-page">
+          <div className="product__container">
+            <div className="product__content">
+              <button className="btn back__btn" onClick={goback}>
+                <FaAngleLeft className="icon" />
+              </button>
+              <div className="image__container">
+                <img src={product.image} />
+              </div>
+              <section className="product__details__section">
+                <p className="title">{product.name}</p>
+                <p>₦ {product.price?.toLocaleString("en-US")}</p>
+                <p className="title">Description</p>
+                <p>{product.description}</p>
+              </section>
             </div>
-            <p>{product.user_name}</p>
-          </section>
-          <section className="contact__details__item contact__details__section">
-            <p className="contact__section__title">Contact details</p>
-            <Link to={`tel:${product.phone}`}>
-              <FaPhone />
-              <p>{product.phone}</p>
-            </Link>
-            <Link to={`mailto:${product.email}`}>
-              <FaEnvelope />
-              <p>{product.email}</p>
-            </Link>
-          </section>
-          <section className="contact__details__item contact__location__section">
-            <p className="contact__section__title">Location</p>
-            <Link>
-              <FaLocationArrow />
-              <p>{product.location}</p>
-            </Link>
-          </section>
+            <div className="contact__container">
+              <section className="contact__details__item contact__image__section">
+                <div className="image__container">
+                  <img src={product.user_image} />
+                </div>
+                <p>{product.user_name}</p>
+              </section>
+              <section className="contact__details__item contact__details__section">
+                <p className="contact__section__title">Contact details</p>
+                <Link to={`tel:${product.phone}`}>
+                  <FaPhone />
+                  <p>{product.phone}</p>
+                </Link>
+                <Link to={`mailto:${product.email}`}>
+                  <FaEnvelope />
+                  <p>{product.email}</p>
+                </Link>
+              </section>
+              <section className="contact__details__item contact__location__section">
+                <p className="contact__section__title">Location</p>
+                <Link>
+                  <FaLocationArrow />
+                  <p>{product.location}</p>
+                </Link>
+              </section>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      }
+    </>
   )
 }
 
